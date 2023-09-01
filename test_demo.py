0b08505dccd81acdc9a9a1966b8858ef146a55a5
srburak <burakkaymaz0101@gmail.com>
2023-09-01 22:07:12 +0300

    day5

--- test_demo.py
+++ test_demo.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.action_chains import ActionChains
+import pytest
 
 class Test_Demo:
 
@@ -22,16 +23,17 @@ class Test_Demo:
     # setup -> test_demo2 -> teardown
     def test_demo2(self):
         assert True
-
-    def test_invalit_login(self):
+    #@pytest.mark.skip()
+    @pytest.mark.parametrize("username,password",[("1","1"),("kullanıcıadım","sifrem")])
+    def test_invalit_login(self,username,password):
          # En fazla 5 saniye olacak şeklide user-name id'li elementin görünmesini bekle 
         WebDriverWait(self.driver,5).until(expected_conditions.visibility_of_element_located((By.ID,"user-name")))
         usernameInput = self.driver.find_element(By.ID , "user-name")
         # En fazla 5 saniye olacak şeklide password id'li elementin görünmesini bekle 
         WebDriverWait(self.driver,5).until(expected_conditions.visibility_of_element_located((By.ID,"password")))
         passwordInput = self.driver.find_element(By.ID , "password")
-        usernameInput.send_keys("1")
-        passwordInput.send_keys("1")
+        usernameInput.send_keys(username)
+        passwordInput.send_keys(password)
         loginBtn = self.driver.find_element(By.ID, "login-button")
         loginBtn.click()
         errorMesage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
